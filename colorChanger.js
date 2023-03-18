447011c58074f30d32393baf860239cbfeb8ccb0
BobAlice21 <122107377+BobAlice21@users.noreply.github.com>
2023-03-18 10:00:09 +0100

    Update and rename colorChanger.js to content.js

--- colorChanger.js
+++
@@ -1,7 +0,0 @@
-// Get all the headline elements on the page
-const headlines = document.querySelectorAll("h1, h2, h3, h4, h5, h6");
-
-// Loop through the headline elements and change their color
-headlines.forEach((headline) => {
-  headline.style.color = "red";
-});
---
+++ content.js
@@ -0,0 +1,5 @@
+// Change the color of all headlines to red
+const headlines = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
+for (let i = 0; i < headlines.length; i++) {
+  headlines[i].style.color = 'red';
+}
